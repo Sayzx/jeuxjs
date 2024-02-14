665797d1acec975a13a5caf33bf79515cb4b4a35
Aylan - Sayzx <74567624+Sayzx@users.noreply.github.com>
2024-02-14 09:28:01 +0100

    Update login.js

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ function checkLogin() {
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
 
-    if (username === 'admin' && password === 'wd9sxevz2rFf#UP@pjghu#wqrm&BG7%#R%') {
+    if (username === 'admin' && password === 'sayzx') {
         localStorage.setItem('loggedIn', true);
         window.location.href = 'game.html'; // Assurez-vous que le chemin vers game.html est correct
     } else {
